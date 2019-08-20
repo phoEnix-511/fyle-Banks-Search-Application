e9bd632e798c092c17e6f9072e933e2c4793ac77
phoEnix-511 <akshay.pipre@yahoo.com>
2019-08-20 19:49:46 +0530

    Initial Commit

---
+++ src/main/java/com/Fyle/BankSearch/BankSearchApplication.java
@@ -0,0 +1,13 @@
+package com.Fyle.BankSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSearchApplication.class, args);
+	}
+
+}
